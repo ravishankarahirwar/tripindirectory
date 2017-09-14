2e4a49acecb4b8f82b3052aea816cccf1cb67ad4
yogesh <yogesh.tikam@tripin.co.in>
2017-09-14 12:21:16 +0530

    Location function added

--- app/src/main/java/directory/tripin/com/tripindirectory/MainActivity.java
+++ app/src/main/java/directory/tripin/com/tripindirectory/MainActivity.java
@@ -1,34 +1,57 @@
 package directory.tripin.com.tripindirectory;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.view.KeyEvent;
-import android.view.View;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.inputmethod.EditorInfo;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.MultiAutoCompleteTextView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.Arrays;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.CommonStatusCodes;
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 import directory.tripin.com.tripindirectory.adapters.PartnersAdapter;
+import directory.tripin.com.tripindirectory.helper.Logger;
 import directory.tripin.com.tripindirectory.manager.PartnersManager;
 import directory.tripin.com.tripindirectory.model.response.GetPartnersResponse;
 import directory.tripin.com.tripindirectory.utils.SpaceTokenizer;
@@ -36,21 +59,24 @@ import directory.tripin.com.tripindirectory.utils.SpaceTokenizer;
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    protected static final int REQUEST_CHECK_SETTINGS = 0x1;
+    private static final int FINE_LOCATION_PERMISSIONS = 1;
+    /**
+     * Represents a geographical location.
+     */
+    protected Location mLastLocation;
     private MultiAutoCompleteTextView mSearchBox;
-    private TextView mSearchResult;
-    private Button mSearch;
-    private String mSearchText;
-
     private Context mContext;
-
     private PartnersManager mPartnerManager;
     private RecyclerView mPartnerList;
-
     private GetPartnersResponse mPartnerListResponse;
     private PartnersAdapter mPartnersAdapter;
-
-    String[] str={"Andoid","Jelly Bean","Froyo",
-            "Ginger Bread","Eclipse Indigo","Eclipse Juno"};
+    /**
+     * Provides the entry point to the Fused Location Provider API.
+     */
+    private FusedLocationProviderClient mFusedLocationClient;
+    private LocationCallback mLocationCallback;
+    private LocationRequest mLocationRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -72,9 +98,28 @@ public class MainActivity extends AppCompatActivity
         mContext = this;
         mPartnerManager = new PartnersManager(mContext);
 
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
-        mSearchBox = (MultiAutoCompleteTextView )this.findViewById(R.id.search_box);
+        mSearchBox = (MultiAutoCompleteTextView) this.findViewById(R.id.search_box);
         mSearchBox.setTokenizer(new SpaceTokenizer());
+
+        mLocationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                for (Location location : locationResult.getLocations()) {
+                    // Update UI with location data
+                    // ...
+                    mLastLocation = location;
+                    Logger.v("Locations :" + location.getLatitude() + "," + location.getLongitude());
+
+                    getLocationName();
+                }
+            }
+
+            ;
+        };
+
+
 
         mSearchBox.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
@@ -89,39 +134,12 @@ public class MainActivity extends AppCompatActivity
         });
 
         mSearchBox.setThreshold(1);
-        mSearchBox.setAdapter( ArrayAdapter.createFromResource(this, R.array.planets_array, android.R.layout.simple_dropdown_item_1line));
-
-
-
-//        mSearchResult = (TextView)this.findViewById(R.id.search_result);
-//
-//        mSearch= (Button) this.findViewById(R.id.search);
-
-//        mSearch.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                mSearchText = mSearchBox.getText().toString();
-////                int toIndex = mSearchText.indexOf("to");
-//                String[] words = mSearchText.split("\\s+");
-//
-//                int index = Arrays.asList(mSearchText.split("\\s+")).indexOf("to");
-//                String source = words[index - 1];
-//                String destination = words[index + 1];
-//
-////                String text = "I love you so much";
-////                String wordToFind = "love";
-////                Pattern word = Pattern.compile(wordToFind);
-////                Matcher match = word.matcher(text);
-////                int found = match.
-//                mSearchResult.setText(source + " , " + destination);
-//
-//            }
-//        });
+        mSearchBox.setAdapter(ArrayAdapter.createFromResource(this, R.array.planets_array, android.R.layout.simple_dropdown_item_1line));
     }
 
     private void init(String enquiry) {
-        mContext = this;
-        mPartnerManager = new PartnersManager(mContext);
+//        mContext = this;
+//        mPartnerManager = new PartnersManager(mContext);
 
         mPartnerList = (RecyclerView) findViewById(R.id.partner_list);
 
@@ -157,8 +175,9 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (id == R.id.location_button) {
+            getLocationsPermission();
+//            return true;
         }
 
         return super.onOptionsItemSelected(item);
@@ -207,4 +226,250 @@ public class MainActivity extends AppCompatActivity
                     }
                 });
     }
+
+
+    private void getLocationsPermission() {
+
+        if (ContextCompat.checkSelfPermission(mContext,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        FINE_LOCATION_PERMISSIONS);
+
+                Logger.v("Coming in rationale");
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        FINE_LOCATION_PERMISSIONS);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+
+                Logger.v("Coming in non rationale");
+            }
+        } else {
+            createLocationRequest();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case FINE_LOCATION_PERMISSIONS: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                    Logger.v("Locations permission : true");
+
+                    createLocationRequest();
+
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Logger.v("Locations permission : false");
+                }
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
+    private void getLocations() {
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            Logger.v("Permissions not granted ");
+        } else {
+            mFusedLocationClient.getLastLocation()
+                    .addOnCompleteListener(this, new OnCompleteListener<Location>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Location> task) {
+                            if (task.isSuccessful() && task.getResult() != null) {
+                                mLastLocation = task.getResult();
+                                Logger.v("Latitude" + mLastLocation.getLatitude());
+                                Logger.v("Longitude" + mLastLocation.getLongitude());
+                            } else {
+                                Logger.v("getLastLocation:exception " + task.getException());
+
+                            }
+                        }
+                    });
+        }
+    }
+
+    private void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(10000);
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationSettings();
+    }
+
+    private void locationSettings() {
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder().addLocationRequest(mLocationRequest);
+
+        SettingsClient client = LocationServices.getSettingsClient(this);
+        Task<LocationSettingsResponse> task = client.checkLocationSettings(builder.build());
+
+        task.addOnSuccessListener(this, new OnSuccessListener<LocationSettingsResponse>() {
+            @Override
+            public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+                // All location settings are satisfied. The client can initialize
+                // location requests here.
+                // ...
+                if (ActivityCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_FINE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED &&
+                        ActivityCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_COARSE_LOCATION)
+                                != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                } else {
+                    mFusedLocationClient.requestLocationUpdates(mLocationRequest,
+                            mLocationCallback,
+                            null /* Looper */);
+
+//                    getLocations();
+                }
+            }
+        });
+
+        task.addOnFailureListener(this, new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                int statusCode = ((ApiException) e).getStatusCode();
+                switch (statusCode) {
+                    case CommonStatusCodes.RESOLUTION_REQUIRED:
+                        // Location settings are not satisfied, but this can be fixed
+                        // by showing the user a dialog.
+                        try {
+                            // Show the dialog by calling startResolutionForResult(),
+                            // and check the result in onActivityResult().
+                            ResolvableApiException resolvable = (ResolvableApiException) e;
+                            resolvable.startResolutionForResult(MainActivity.this,
+                                    REQUEST_CHECK_SETTINGS);
+                        } catch (IntentSender.SendIntentException sendEx) {
+                            // Ignore the error.
+                        }
+                        break;
+                    case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                        // Location settings are not satisfied. However, we have no way
+                        // to fix the settings so we won't show the dialog.
+                        break;
+                }
+            }
+        });
+
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == 1) {
+
+            if (resultCode == RESULT_OK) {
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED &&
+                        ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                                != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                } else {
+                    mFusedLocationClient.requestLocationUpdates(mLocationRequest,
+                            mLocationCallback,
+                            null /* Looper */);
+                }
+            }
+
+            if (resultCode == RESULT_CANCELED) {
+                //Write your code if there's no result
+                Logger.v("permission for gps denied");
+            }
+        }
+    }
+
+    private void getLocationName() {
+        Geocoder geocoder = new Geocoder(mContext, Locale.getDefault());
+        String result = null;
+        try {
+            List<Address> addressList = geocoder.getFromLocation(
+                    mLastLocation.getLatitude(), mLastLocation.getLongitude(), 1);
+            if (addressList != null && addressList.size() > 0) {
+                Address address = addressList.get(0);
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < address.getMaxAddressLineIndex(); i++) {
+                    sb.append(address.getAddressLine(i)).append("\n");
+                }
+                sb.append(address.getLocality()).append("\n");
+                sb.append(address.getPostalCode()).append("\n");
+                sb.append(address.getCountryName());
+                result = sb.toString();
+
+//                Logger.v("Address : " + result);
+//                Logger.v("Locale "+address.getLocale().toString());
+                Logger.v("Locality "+address.getLocality());
+                Logger.v("SubLocality "+address.getSubLocality());
+//                Logger.v("Address line One " + address.getAddressLine(0));
+//                Logger.v("Postal code " + address.getPostalCode());
+//                Logger.v("FeatureName"+address.getFeatureName());
+//                Logger.v("Premises"+address.getPremises());
+//                Logger.v("Admin Area"+address.getAdminArea());
+
+                if(mSearchBox.getText().length() == 0){
+                    mSearchBox.setText(address.getLocality());
+                    init(mSearchBox.getText().toString());
+                    stopLocationUpdates();
+                }
+            }
+        } catch (IOException e) {
+            Logger.v("Unable connect to Geocoder " + e);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        stopLocationUpdates();
+    }
+
+    private void stopLocationUpdates() {
+        mFusedLocationClient.removeLocationUpdates(mLocationCallback);
+    }
+
 }
