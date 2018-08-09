87a8fcb35b78794efd978b876c06667fdab5583f
ravishankar.ahirwar <shubham.sardar@tripin.co.in>
2018-08-09 15:32:45 +0530

    functions deployed

--- functions/index.js
+++ functions/index.js
@@ -8,7 +8,7 @@ admin.initializeApp();
 //  response.send("Hello from Firebase!");
 // });
 
-exports.makeUppercase = functions.database.ref('/posts/{pushId}')
+exports.newLoadpost = functions.database.ref('/posts/{pushId}')
     .onCreate((snapshot, context) => {
       // Grab the current value of what was written to the Realtime Database.
       const original = snapshot.val();
@@ -30,3 +30,88 @@ exports.makeUppercase = functions.database.ref('/posts/{pushId}')
         };
          return admin.messaging().sendToTopic("loadboardNotification", payload, options);
     });
+
+//Make the initial mAccountStatusValue 0
+exports.emptyCompanyCreated = functions.firestore
+  .document('partners/{documentId}')
+  .onCreate(event => {
+      return event.data.ref.update({ mAccountStatus: 0 });
+});
+
+
+exports.newVerifiedCompany = functions.database.ref('/userCategories/companyVerifiedUsers/{pushId}')
+    .onCreate(event => {
+      // Grab the current value of what was written to the Realtime Database.
+      var newValue = event.data.val();
+	  if(newValue.mFcmToken !==null){
+		   console.log('Sending Verified Notification', event.params.pushId, newValue);
+	  // Create a DATA notification
+    const payload = {
+       data: {
+        type: "2",
+        mCompanyName: newValue.mCompanyName,
+      }
+    };
+    const options = {
+        priority: "high",
+        timeToLive: 60 * 60 * 24
+    };
+
+
+     return admin.messaging().sendToDevice(newValue.mFcmToken, payload,options).then((response)=> {
+        return console.info("Successfully sent notification")
+      }).catch(function(error) {
+
+        return console.warn("Error sending notification " , error)
+         });
+	  }else{
+	        throw new Error("Token Null")
+	  }
+
+    });
+
+	exports.newRejectedCompany = functions.database.ref('/userCategories/companyRejectedUsers/{pushId}')
+    .onCreate(event => {
+      // Grab the current value of what was written to the Realtime Database.
+      var newValue = event.data.val();
+	  if(newValue.mFcmToken !==null){
+	  // Create a DATA notification
+    const payload = {
+       data: {
+        type: "3",
+      }
+    };
+    const options = {
+        priority: "high",
+        timeToLive: 60 * 60 * 24
+    };
+
+    return admin.messaging().sendToDevice(newValue.mFcmToken, payload,options).then((response)=> {
+        return console.info("Successfully sent notification")
+      }).catch(function(error) {
+        return console.warn("Error sending notification " , error)
+         });
+	  }
+    });
+
+
+	//one to one message notification
+exports.onetoonechat = functions.firestore
+  .document('chats/chatrooms/{chatroomId}/{messageId}')
+  .onCreate(event => {
+    var newValue = event.data.data();
+
+	// Create a DATA notification
+    const payload = {
+       data: {
+        type: "6",
+		chatroomId: event.params.chatroomId,
+        docId: event.params.messageId
+      }
+    };
+
+	 console.log('Sending chat notification', event.params.chatroomId, newValue);
+
+    return admin.messaging().sendToDevice(newValue.mReciversFcmToken, payload);
+});
+
